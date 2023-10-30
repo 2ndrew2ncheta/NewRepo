2b2595995bcc1e397a7b967a1d676f35a0ce6470
2ndrew2ncheta <113394193+2ndrew2ncheta@users.noreply.github.com>
2023-10-30 15:16:38 -0400

    Update Coolcalcs.py

--- mathFunctions/Coolcalcs.py
+++ mathFunctions/Coolcalcs.py
@@ -1,5 +1,11 @@
 def addNumbers(number1, number2):
+	'''
+	Adds 2 numbers
+	'''
 	return number1+number2
-	
+
 def subtractNumbers(number1, number2):
+	'''
+	Returns n1 - n2
+	'''
 	return number1-number2
